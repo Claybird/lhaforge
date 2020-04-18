2464d6691340a9910bf56e79afb6686f9b1b1f8f
Claybird <claybird.without.wing@gmail.com>
2020-04-18 22:13:11 +0900

    added test cases

--- LhaForge/test/test_extract.cpp
+++ LhaForge/test/test_extract.cpp
@@ -1,5 +1,6 @@
 ﻿#include "stdafx.h"
 #include "CppUnitTest.h"
+#include "extract.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -48,6 +49,35 @@ namespace UnitTest
 			//full-width space
 			Assert::AreEqual(std::wstring(L"123abc"), trimArchiveName(true, L"123abc456　"));
 			Assert::AreEqual(std::wstring(L"123abc456"), trimArchiveName(false, L"123abc456　"));
+		}
+
+		TEST_METHOD(test_determineExtractBaseDir) {
+			std::wstring determineExtractBaseDir(const wchar_t* archive_path, LF_EXTRACT_ARGS& args);
+			LF_EXTRACT_ARGS fakeArg;
+			fakeArg.extract.OutputDirType = OUTPUT_TO::OUTPUT_TO_SPECIFIC_DIR;
+			fakeArg.extract.OutputDirUserSpecified = std::filesystem::current_path().c_str();
+			fakeArg.general.WarnNetwork = FALSE;
+			fakeArg.general.WarnRemovable = FALSE;
+			fakeArg.general.OnDirNotFound = LOSTDIR_FORCE_CREATE;
+
+			auto out = determineExtractBaseDir(L"path_to_archive/archive.ext", fakeArg);
+			Assert::AreEqual(std::wstring(std::filesystem::current_path().c_str()), out);
+		}
+		TEST_METHOD(test_determineExtractDir) {
+			std::wstring determineExtractDir(const wchar_t* archive_path, const wchar_t* output_base_dir, const LF_EXTRACT_ARGS& args);
+			LF_EXTRACT_ARGS fakeArg;
+			fakeArg.extract.CreateDir = CREATE_OUTPUT_DIR_NEVER;
+			fakeArg.extract.RemoveSymbolAndNumber = false;
+			Assert::AreEqual(std::wstring(L"path_to_output"), 
+				replace(determineExtractDir(L"path_to_archive/archive.ext", L"path_to_output", fakeArg), L"\\", L"/"));
+			Assert::AreEqual(std::wstring(L"path_to_output"),
+				replace(determineExtractDir(L"path_to_archive/archive  .ext", L"path_to_output", fakeArg), L"\\", L"/"));
+
+			fakeArg.extract.CreateDir = CREATE_OUTPUT_DIR_ALWAYS;
+			Assert::AreEqual(std::wstring(L"path_to_output/archive"),
+				replace(determineExtractDir(L"path_to_archive/archive.ext", L"path_to_output", fakeArg), L"\\", L"/"));
+			Assert::AreEqual(std::wstring(L"path_to_output/archive"),
+				replace(determineExtractDir(L"path_to_archive/archive  .ext", L"path_to_output", fakeArg), L"\\", L"/"));
 		}
 	};
 }
