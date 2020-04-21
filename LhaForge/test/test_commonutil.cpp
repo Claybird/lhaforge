5c2dc74c4a22fd979d7cc16a0e3cd687840ddf71
Claybird <claybird.without.wing@gmail.com>
2020-04-21 23:20:11 +0900

    added test

---
+++ LhaForge/test/test_commonutil.cpp
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "CommonUtil.h"
+#include "ConfigCode/ConfigGeneral.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(CommonUtil)
+	{
+	public:
+		TEST_METHOD(test_LF_get_output_dir) {
+			struct LF_GET_OUTPUT_DIR_TEST_CALLBACK :I_LF_GET_OUTPUT_DIR_CALLBACK {
+				std::wstring _default_path;
+				void setArchivePath(const wchar_t* archivePath) {
+					if (_default_path.empty()) {
+						_default_path = std::filesystem::path(archivePath).parent_path();
+					}
+				}
+				std::wstring operator()()override {
+					return _default_path;
+				}
+			};
+			LF_GET_OUTPUT_DIR_TEST_CALLBACK output_dir_callback;
+			output_dir_callback.setArchivePath(L"C:/path_to/test_archive.ext");
+			auto outputDir = LF_get_output_dir(OUTPUT_TO_SAME_DIR, L"C:/path_to/test_archive.ext", L"", output_dir_callback);
+			Assert::AreEqual(outputDir, std::wstring(L"C:/path_to"));
+		}
+		TEST_METHOD(test_LF_confirm_output_dir_type) {
+			CConfigGeneral conf;
+			conf.WarnRemovable = false;
+			conf.WarnNetwork = false;
+			Assert::AreEqual(true, LF_confirm_output_dir_type(conf, L"C:/"));
+			Assert::AreEqual(true, LF_confirm_output_dir_type(conf, L"C:/temp"));
+		}
+	};
+};
