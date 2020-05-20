7abe62a9f83996236047f24337821fe67c10eeac
Claybird <claybird.without.wing@gmail.com>
2020-05-20 23:47:32 +0900

    added test cases for extractOneArchive/testOneArchive

--- LhaForge/test/test_extract.cpp
+++ LhaForge/test/test_extract.cpp
@@ -42,6 +42,7 @@ TEST(extract, determineExtractBaseDir) {
 	auto out = determineExtractBaseDir(L"path_to_archive/archive.ext", fakeArg);
 	EXPECT_EQ(std::wstring(std::filesystem::current_path().c_str()), out);
 }
+
 TEST(extract, determineExtractDir) {
 	std::wstring determineExtractDir(const wchar_t* archive_path, const wchar_t* output_base_dir, const LF_EXTRACT_ARGS& args);
 	LF_EXTRACT_ARGS fakeArg;
@@ -57,6 +58,91 @@ TEST(extract, determineExtractDir) {
 		replace(determineExtractDir(L"path_to_archive/archive.ext", L"path_to_output", fakeArg), L"\\", L"/"));
 	EXPECT_EQ(L"path_to_output/archive",
 		replace(determineExtractDir(L"path_to_archive/archive  .ext", L"path_to_output", fakeArg), L"\\", L"/"));
+}
+
+TEST(extract, extractOneArchive) {
+	_wsetlocale(LC_ALL, L"");	//default locale
+
+	auto tempDir = std::filesystem::path(UtilGetTempPath() + L"test_extractOneArchive");
+	UtilDeleteDir(tempDir, true);
+	EXPECT_FALSE(std::filesystem::exists(tempDir));
+	std::filesystem::create_directories(tempDir);
+	auto archiveFile = std::filesystem::path(__FILEW__).parent_path() / L"test_extract.zip";
+	ASSERT_TRUE(std::filesystem::exists(archiveFile));
+
+	ARCLOG arcLog;
+	EXPECT_NO_THROW(
+		extractOneArchive(archiveFile, tempDir, arcLog,
+		[&](const std::wstring& fullpath, const LF_ARCHIVE_ENTRY* entry) {return overwrite_options::abort; },
+		[&](const std::wstring& originalPath, UINT64 currentSize, UINT64 totalSize) {}
+	));
+
+	EXPECT_TRUE(std::filesystem::exists(tempDir / L"dirA"));
+	EXPECT_TRUE(std::filesystem::exists(tempDir / L"dirA/dirB"));
+	EXPECT_TRUE(std::filesystem::exists(tempDir / L"dirA/dirB/file2.txt"));
+	EXPECT_TRUE(std::filesystem::exists(tempDir / L"dirA/dirB/dirC"));
+	EXPECT_TRUE(std::filesystem::exists(tempDir / L"dirA/dirB/dirC/file1.txt"));
+	EXPECT_TRUE(std::filesystem::exists(tempDir / L"かきくけこ"));
+	EXPECT_TRUE(std::filesystem::exists(tempDir / L"かきくけこ/file3.txt"));
+	EXPECT_TRUE(std::filesystem::exists(tempDir / L"あいうえお.txt"));
+
+	UtilDeleteDir(tempDir, true);
+	EXPECT_FALSE(std::filesystem::exists(tempDir));
+}
+
+TEST(extract, extractOneArchive_broken_files) {
+	_wsetlocale(LC_ALL, L"");	//default locale
+
+	const std::vector<std::wstring> files = { L"test_broken_file.zip" , L"test_broken_crc.zip" };
+
+	for (const auto& file : files) {
+		auto tempDir = std::filesystem::path(UtilGetTempPath() + L"test_extractOneArchive");
+		UtilDeleteDir(tempDir, true);
+		EXPECT_FALSE(std::filesystem::exists(tempDir));
+		std::filesystem::create_directories(tempDir);
+		auto archiveFile = std::filesystem::path(__FILEW__).parent_path() / file;
+		ASSERT_TRUE(std::filesystem::exists(archiveFile));
+
+		ARCLOG arcLog;
+		EXPECT_THROW(
+			extractOneArchive(archiveFile, tempDir, arcLog,
+				[&](const std::wstring& fullpath, const LF_ARCHIVE_ENTRY* entry) {return overwrite_options::abort; },
+				[&](const std::wstring& originalPath, UINT64 currentSize, UINT64 totalSize) {}
+		), LF_EXCEPTION);
+
+		UtilDeleteDir(tempDir, true);
+		EXPECT_FALSE(std::filesystem::exists(tempDir));
+	}
+}
+
+TEST(extract, testOneArchive) {
+	_wsetlocale(LC_ALL, L"");	//default locale
+
+	auto archiveFile = std::filesystem::path(__FILEW__).parent_path() / L"test_extract.zip";
+	ASSERT_TRUE(std::filesystem::exists(archiveFile));
+
+	ARCLOG arcLog;
+	EXPECT_NO_THROW(
+	testOneArchive(archiveFile, arcLog,
+		[&](const std::wstring& originalPath, UINT64 currentSize, UINT64 totalSize) {}
+	));
+}
+
+TEST(extract, testOneArchive_broken_files) {
+	_wsetlocale(LC_ALL, L"");	//default locale
+
+	const std::vector<std::wstring> files = { L"test_broken_file.zip" , L"test_broken_crc.zip" };
+
+	for (const auto& file : files) {
+		auto archiveFile = std::filesystem::path(__FILEW__).parent_path() / file;
+		ASSERT_TRUE(std::filesystem::exists(archiveFile));
+
+		ARCLOG arcLog;
+		EXPECT_THROW(
+			testOneArchive(archiveFile, arcLog,
+				[&](const std::wstring& originalPath, UINT64 currentSize, UINT64 totalSize) {}
+		), LF_EXCEPTION);
+	}
 }
 
 #endif
