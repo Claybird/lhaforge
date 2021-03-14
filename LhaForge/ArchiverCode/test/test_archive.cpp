a957a120165c73d57a836e9190710d7d3925ebc5
Claybird <claybird.without.wing@gmail.com>
2021-03-14 14:06:57 +0900

    fixed path and expected result

--- LhaForge/ArchiverCode/test/test_archive.cpp
+++ LhaForge/ArchiverCode/test/test_archive.cpp
@@ -5,18 +5,18 @@
 
 TEST(CLFArchive, is_known_format)
 {
-	const auto dir = LF_PROJECT_DIR();
+	const auto dir = LF_PROJECT_DIR() / L"test";
 
 	EXPECT_FALSE(CLFArchive::is_known_format(__FILEW__));
 	EXPECT_TRUE(CLFArchive::is_known_format(dir / L"test_extract.zip"));
 	EXPECT_TRUE(CLFArchive::is_known_format(dir / L"test_broken_crc.zip"));
-	EXPECT_FALSE(CLFArchive::is_known_format(dir / L"test_broken_file.zip"));
+	EXPECT_TRUE(CLFArchive::is_known_format(dir / L"test_broken_file.zip"));
 	EXPECT_FALSE(CLFArchive::is_known_format(L"some_non_existing_file"));
 }
 
 TEST(CLFArchive, get_num_entries)
 {
-	const auto dir = LF_PROJECT_DIR();
+	const auto dir = LF_PROJECT_DIR() / L"test";
 
 	CLFArchive arc;
 	arc.read_open(dir / L"test_extract.zip", CLFPassphraseNULL());
