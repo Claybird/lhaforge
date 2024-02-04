33b85237a7b87899bdc371df27d0a1ccfa5277f2
Claybird <claybird.without.wing@gmail.com>
2024-02-04 14:27:05 +0900

    fixed test for CSemaphoreLocker

--- LhaForge/Utilities/Semaphore.cpp
+++ LhaForge/Utilities/Semaphore.cpp
@@ -6,11 +6,14 @@ TEST(semaphore, semaphore) {
 	CSemaphoreLocker lock1;
 	CSemaphoreLocker lock2;
 	CSemaphoreLocker lock3;
-	EXPECT_TRUE(lock1.Lock(L"test_semaphore", 2));
-	EXPECT_TRUE(lock2.Lock(L"test_semaphore", 2));
-	//EXPECT_FALSE(lock3.Lock(L"test_semaphore", 2));	will wait forever
+	EXPECT_TRUE(lock1.Create(L"test_semaphore", 2));
+	EXPECT_TRUE(lock1.Lock(INFINITE));
+	EXPECT_TRUE(lock2.Create(L"test_semaphore", 2));
+	EXPECT_TRUE(lock2.Lock(INFINITE));
+	EXPECT_TRUE(lock3.Create(L"test_semaphore", 2));
+	EXPECT_FALSE(lock3.Lock(10));
 	lock1.Release();
-	EXPECT_TRUE(lock3.Lock(L"test_semaphore", 2));
+	EXPECT_TRUE(lock3.Lock(10));
 }
 
 #endif
