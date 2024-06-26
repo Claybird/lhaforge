d3a5076d2db2100c488c8896e566283ec66bd3cf
Claybird <claybird.without.wing@gmail.com>
2024-06-26 22:23:23 +0900

    keep dialog while archive scan continues

--- LhaForge/extract.cpp
+++ LhaForge/extract.cpp
@@ -701,8 +701,11 @@ bool GUI_extract_multiple_files(
 			arc.read_open(archive_path, std::make_shared<CLFPassphraseGUI>());
 
 			//output destination directory [could be same as the output base directory]
-			output_dir = determineExtractDir(arc, CLFScanProgressHandlerGUI(NULL), archive_path, output_base_dir, args);
-			progressHandler.setNumEntries(arc.get_num_entries());
+			{
+				CLFScanProgressHandlerGUI progress(NULL);
+				output_dir = determineExtractDir(arc, progress, archive_path, output_base_dir, args);
+				progressHandler.setNumEntries(arc.get_num_entries());
+			}
 
 			//make sure output directory exists
 			try {
