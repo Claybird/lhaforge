e3feb34c3d7528dd7b7aec5176c494969412831a
Claybird <claybird.without.wing@gmail.com>
2024-11-09 17:26:29 +0900

    added test for zip and zipx 2099 test for minizip-ng

--- LhaForge/ArchiverCode/test/test_archive_zip.cpp
+++ LhaForge/ArchiverCode/test/test_archive_zip.cpp
@@ -362,6 +362,108 @@ TEST(CLFArchiveZIP, read_enum_sfx)
 	EXPECT_EQ(nullptr, entry);
 }
 
+TEST(CLFArchiveZIP, read_enum_2099_zip)
+{
+	_wsetlocale(LC_ALL, L"");	//default locale
+	const auto file = std::filesystem::path(__FILEW__).parent_path() / L"test_2099.zip";
+
+	CLFArchiveZIP a;
+	auto pp = std::make_shared<CLFPassphraseNULL>();
+	a.read_open(file, pp);
+	EXPECT_TRUE(a.is_modify_supported());
+	ASSERT_EQ(L"ZIP", a.get_format_name());
+
+	int count = 0;
+	int numDir = 0;
+	for (auto entry = a.read_entry_begin(); entry; entry = a.read_entry_next()) {
+		count++;
+		if (entry->is_directory()) {
+			numDir++;
+		} else {
+			if (entry->path.wstring().find(L"ccd.txt") != -1) {
+				ASSERT_EQ(entry->stat.st_size, 44);
+				//EXPECT_EQ(entry->method_name, L"---");
+				ASSERT_EQ(entry->compressed_size, 44);
+				std::vector<char> data;
+				for (;;) {
+					bool bEOF = false;
+					a.read_file_entry_block([&](const void* buf, size_t data_size, const offset_info* offset) {
+						//EXPECT_EQ(nullptr, offset);
+						if (buf) {
+							data.insert(data.end(), (const char*)buf, ((const char*)buf) + data_size);
+						} else {
+							bEOF = true;
+						}
+					});
+					if (bEOF) {
+						break;
+					}
+				}
+				EXPECT_EQ(data.size(), entry->stat.st_size);
+				EXPECT_EQ(std::string(data.begin(), data.end()), ";kljd;lfj;lsdahg;has:hn:h :ahsd:fh:asdhg:ioh");
+			} else {
+				EXPECT_EQ(entry->stat.st_size, 48);
+				//EXPECT_EQ(entry->method_name, L"---");
+				ASSERT_EQ(entry->compressed_size, 46);
+			}
+		}
+		EXPECT_FALSE(entry->is_encrypted);
+	}
+	EXPECT_EQ(count, 2099);
+	EXPECT_EQ(numDir, 0);
+}
+
+TEST(CLFArchiveZIP, read_enum_2099_zipx)
+{
+	_wsetlocale(LC_ALL, L"");	//default locale
+	const auto file = std::filesystem::path(__FILEW__).parent_path() / L"test_2099.zipx";
+
+	CLFArchiveZIP a;
+	auto pp = std::make_shared<CLFPassphraseNULL>();
+	a.read_open(file, pp);
+	EXPECT_TRUE(a.is_modify_supported());
+	ASSERT_EQ(L"ZIP", a.get_format_name());
+
+	int count = 0;
+	int numDir = 0;
+	for (auto entry = a.read_entry_begin(); entry; entry = a.read_entry_next()) {
+		count++;
+		if (entry->is_directory()) {
+			numDir++;
+		} else {
+			if (entry->path.wstring().find(L"ccd.txt") != -1) {
+				ASSERT_EQ(entry->stat.st_size, 44);
+				//EXPECT_EQ(entry->method_name, L"---");
+				EXPECT_EQ(entry->compressed_size, 40);
+				std::vector<char> data;
+				for (;;) {
+					bool bEOF = false;
+					a.read_file_entry_block([&](const void* buf, size_t data_size, const offset_info* offset) {
+						//EXPECT_EQ(nullptr, offset);
+						if (buf) {
+							data.insert(data.end(), (const char*)buf, ((const char*)buf) + data_size);
+						} else {
+							bEOF = true;
+						}
+					});
+					if (bEOF) {
+						break;
+					}
+				}
+				EXPECT_EQ(data.size(), entry->stat.st_size);
+				EXPECT_EQ(std::string(data.begin(), data.end()), ";kljd;lfj;lsdahg;has:hn:h :ahsd:fh:asdhg:ioh");
+			} else {
+				ASSERT_EQ(entry->stat.st_size, 48);
+				//EXPECT_EQ(entry->method_name, L"---");
+				ASSERT_EQ(entry->compressed_size, 41);
+			}
+		}
+		EXPECT_FALSE(entry->is_encrypted);
+	}
+	EXPECT_EQ(count, 2099 + 1);
+	EXPECT_EQ(numDir, 1);
+}
+
 TEST(CLFArchiveZIP, read_passphrase)
 {
 	_wsetlocale(LC_ALL, L"");	//default locale
@@ -494,7 +596,7 @@ TEST(CLFArchiveZIP, is_known_format)
 		EXPECT_FALSE(CLFArchiveZIP::is_known_format(dir / L"image_method0.arj"));
 		EXPECT_FALSE(CLFArchiveZIP::is_known_format(dir / L"test.bza"));
 		EXPECT_FALSE(CLFArchiveZIP::is_known_format(dir / L"test.gza"));
-		EXPECT_FALSE(CLFArchiveZIP::is_known_format(dir / L"test_zip_sfx.dat"));	//true if checked inside content
+		EXPECT_TRUE(CLFArchiveZIP::is_known_format(dir / L"test_zip_sfx.dat"));	//true if checked inside content
 	}
 }
 
